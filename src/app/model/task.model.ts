52ebad4ef837b62a3acf54639eb11155c48c8315
Nikola Malinovic <nikolamalinovic97@gmail.com>
2019-06-19 15:30:48 +0200

    Added dummy Tasks in task-list component. Also updated Task model.

--- src/app/model/task.model.ts
+++ src/app/model/task.model.ts
@@ -1,10 +1,10 @@
 export class Task {
 
-    public id: number;
-    public title: string;
-    public text: string;
-    public dateTime: string;
-    public checked: boolean;
+    private id: number;
+    private title: string;
+    private text: string;
+    private dateTime: string;
+    private checked: boolean;
 
     constructor(id: number, title: string, text: string, dateTime: string, checked: boolean) {
         this.id = id;
@@ -12,5 +12,41 @@ export class Task {
         this.text = text;
         this.dateTime = dateTime;
         this.checked = checked;
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    getTitle(): string {
+        return this.title;
+    }
+
+    setTitle(title: string) {
+        this.title = title;
+    }
+
+    getText(): string {
+        return this.text;
+    }
+
+    setText(text: string) {
+        this.text = text;
+    }
+
+    getDateTime(): string {
+        return this.dateTime;
+    }
+
+    setDateTime(dateTime: string) {
+        this.dateTime = dateTime;
+    }
+
+    isChecked(): boolean {
+        return this.checked;
+    }
+
+    changeChecked() {
+        this.checked = !this.checked;
     }
 }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../model/task.model';
 
 @Component({
   selector: 'app-task-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
+
+  // this is a dummy tasks variable
+  // i will add real tasks from mysql database in later stages via API (httpclient)
+  tasks: Task[] = [
+    new Task(1, 'Test1', 'Some text 1', '2019-06-14 19:15:12', false),
+    new Task(2, 'Test2', 'Some text 2', '2019-06-18 11:15:12', false),
+    new Task(3, 'Test3', 'Some text 3', '2019-06-19 13:15:12', true)
+  ];
 
   constructor() { }
 
