834d6dafa21c8e8dbdd6e8d7f8546ff40ab024fb
Nikola Malinovic <nikolamalinovic97@gmail.com>
2019-06-20 12:11:30 +0200

    Finnished template for 'task-list' component.

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -13,7 +13,13 @@ export class TaskListComponent implements OnInit {
   tasks: Task[] = [
     new Task(1, 'Test1', 'Some text 1', '2019-06-14 19:15:12', false),
     new Task(2, 'Test2', 'Some text 2', '2019-06-18 11:15:12', false),
-    new Task(3, 'Test3', 'Some text 3', '2019-06-19 13:15:12', true)
+    new Task(3, 'Test3', 'Some text 3', '2019-06-19 13:15:12', true),
+    new Task(4, 'Test1', 'Some text 1', '2019-06-14 19:15:12', false),
+    new Task(5, 'Test2', 'Some text 2', '2019-06-18 11:15:12', false),
+    new Task(6, 'Test3', 'Some text 3', '2019-06-19 13:15:12', true),
+    new Task(7, 'Test1', 'Some text 1', '2019-06-14 19:15:12', false),
+    new Task(8, 'Test2', 'Some text 2', '2019-06-18 11:15:12', false),
+    new Task(9, 'Test3', 'Some text 3', '2019-06-19 13:15:12', true)
   ];
 
   constructor() { }
