3fff442d7350e71874785f50aa8220c766c01e38
Nikola Malinovic <nikolamalinovic97@gmail.com>
2019-06-22 13:37:30 +0200

    Generated new component called 'add-task', which is going to help user creat a new task via form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TaskListComponent
+    TaskListComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
